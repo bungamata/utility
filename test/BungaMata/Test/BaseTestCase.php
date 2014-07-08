3b80f05578de81b189185f136d2a9a37660fb8d2
GusDeCooL <gusdecool@gmail.com>
2014-07-09 03:33:39 +0800

    add Utility\Math

--- test/BungaMata/Test/BaseTestCase.php
+++ test/BungaMata/Test/BaseTestCase.php
@@ -4,10 +4,12 @@
 
 namespace BungaMata\Test;
 
+use PHPUnit_Framework_TestCase;
+
 /**
  * Base test class for all test case
  * @package BungaMata\Test
  */
-abstract class BaseTestCase extends \PHPUnit_Framework_TestCase {
+abstract class BaseTestCase extends PHPUnit_Framework_TestCase {
 
 }
